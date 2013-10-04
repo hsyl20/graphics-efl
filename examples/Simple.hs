c9b0747b55349b57baf62ac71231dc5469a7abfc
Sylvain HENRY <hsyl20@gmail.com>
2013-10-04 18:59:07 +0200

    Use -threaded

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -1,30 +1,22 @@
-import Graphics.Efl.Canvas
-import Graphics.Efl.Window
-import Graphics.Efl.Core
-import Control.Concurrent
+import Graphics.Efl.Simple
 
 main :: IO ()
 main = do
-   initWindowingSystem
-   window <- createWindow Nothing 0 0 800 600 Nothing
-   canvas <- getWindowCanvas window
-   showWindow window
-   
-   _ <- forkIO beginMainLoop
+   withDefaultWindow $ \ _ canvas -> do
 
-   r <- addRectangle canvas
-   resize r 100 40
-   move r 20 40
-   setColor r 255 0 0 255
-   uncover r
+      r <- addRectangle canvas
+      resize r 100 40
+      move r 20 40
+      setColor r 255 0 0 255
+      uncover r
 
-   t <- addText canvas
-   setTextStyle t EvasTextStylePlain EvasTextStyleShadowDirectionBottomRight
-   setTextFont t "DejaVu" 14
-   resize t 100 50
-   move t 25 50
-   setColor t 0 255 0 255
-   setText t "Haskell-EFL!!"
-   uncover t
+      t <- addText canvas
+      setText t "Haskell-EFL!!"
+      resize t 200 100
+      move t 25 50
+      setTextStyle t EvasTextStylePlain EvasTextStyleShadowDirectionBottomRight
+      setTextFont t "DejaVu" 14
+      setColor t 0 255 0 255
+      uncover t
 
-   return ()
+      renderCanvas canvas
--- src/Graphics/Efl/Simple.hs
+++ src/Graphics/Efl/Simple.hs
@@ -1,6 +1,6 @@
 module Graphics.Efl.Simple (
    module M,
-   defaultWindow
+   defaultWindow, defaultWindowEx, withDefaultWindow
 ) where
 
 import Graphics.Efl.Window as M
@@ -9,7 +9,19 @@ import Graphics.Efl.Core
 
 import Control.Concurrent
 
--- | Initialize a default window
+-- | Initialize a default window. Use the MVar to block the main thread if necessary
+defaultWindowEx :: IO (Window,Canvas,MVar ())
+defaultWindowEx = do
+   initWindowingSystem
+   w <- createWindow Nothing 0 0 800 600 Nothing
+   c <- getWindowCanvas w
+   showWindow w
+   v <- newEmptyMVar
+   _ <- forkFinally beginMainLoop (\_ -> putMVar v ())
+   return (w,c,v)
+
+-- | Initialize a default window.
+--The main thread must not be killed for it to continue to work
 defaultWindow :: IO (Window,Canvas)
 defaultWindow = do
    initWindowingSystem
@@ -18,3 +30,11 @@ defaultWindow = do
    showWindow w
    _ <- forkIO beginMainLoop
    return (w,c)
+
+-- | Create a default window and block
+withDefaultWindow :: (Window -> Canvas -> IO a) -> IO a
+withDefaultWindow f = do
+   (w,c,v) <- defaultWindowEx
+   r <- f w c
+   readMVar v
+   return r

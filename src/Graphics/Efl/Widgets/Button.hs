f14f90d9e91ed8e3ff19f15fe2a011c5cfa9152f
Sylvain HENRY <hsyl20@gmail.com>
2013-11-26 03:39:03 +0100

    Add button widget

---
+++ src/Graphics/Efl/Widgets/Button.hs
@@ -0,0 +1,47 @@
+module Graphics.Efl.Widgets.Button where
+
+import Graphics.Efl.Widgets.Reactive
+import Graphics.Efl.Widgets.Window
+import Graphics.Efl.Widgets.Rectangle
+import Control.Applicative
+
+data Button = Button {
+   buttonBackground :: Rectangle,
+
+   buttonPosition :: Property (Int,Int),
+   buttonSize :: Property (Int,Int),
+   buttonVisible :: Property Bool,
+
+   buttonPressed :: Property Bool
+}
+
+
+createButton :: Window -> IO Button
+createButton win = do
+
+   bg <- createRectangle win
+
+   -- Button pressed automaton
+   let 
+      unpressed = Auto False [
+            Transition (rectangleMouseDown bg) (const pressed)
+         ]
+      pressed = Auto True [
+            Transition (rectangleMouseUp bg) (const unpressed)
+         ]
+
+   btn <- Button bg
+      <$> newIORefProperty (0,0)
+      <*> newIORefProperty (40,15)
+      <*> newIORefProperty True
+      <*> runAutomaton unpressed id
+
+   rectangleSize bg =& readProperty (buttonSize btn)
+   rectanglePosition bg =& readProperty (buttonPosition btn)
+   rectangleVisible bg =& readProperty (buttonVisible btn)
+
+   rectangleColor bg =& do
+      press <- readProperty (buttonPressed btn)
+      return $ if press then (200,200,200,255) else (240,240,240,255)
+
+   return btn
